6289c3032a03df26a81b505665b6b0934efad2cb
Omkar Arora <aroraomkar@gmail.com>
2021-04-02 12:39:55 +0530

    feat: header logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
 import { Navbar } from "../Navbar/Navbar";
+import logo from "../../images/sword.svg"
 import "./header.css";
 
 export const Header = ({ active }) => {
   return (
     <header className="header">
-      <div className="container-logo">LOGO</div>
+      <div className="container-logo">
+		  <img src={logo} alt="logo"/>
+		  Game Sense
+	  </div>
       <Navbar active={active} />
       <div className="container-user-details">
         <div>
