9d1c0aa1f6dfca5df7d3302d26e519ae05011147
Omkar Arora <aroraomkar@gmail.com>
2021-04-26 20:22:28 +0530

    style: styling for productdetails page

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,23 +1,26 @@
 import { Header } from "../Header/Header";
 import { NavPhone } from "../NavPhone/NavPhone";
-import { useParams, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { useWindowSize } from "../../hooks";
-import { useNavPhone } from "../../contexts";
+import { useNavPhone, useCart, useWishlist, useAlert } from "../../contexts";
+import { IoBagCheckOutline } from "react-icons/io5";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 import "./productDetails.css";
 
 export const ProductDetails = () => {
-  const { productId } = useParams();
+  // const { productId } = useParams();
   const screenWidth = useWindowSize().width;
   const { navPhoneVisible } = useNavPhone();
   const { state } = useLocation();
   const { item } = state;
-  console.log({ state });
+  const { cart, cartDispatch } = useCart();
+  const { wishlist, wishlistDispatch } = useWishlist();
+  const { setSnackbar } = useAlert();
+
   return (
     <div className="container-app container-product-details">
       {screenWidth < 768 && navPhoneVisible && <NavPhone active="" />}
       <Header active="" />
-      <div className="page-heading">Product Details</div>
-      Product Id: {productId}
       <main className="main-product-details">
         <div className="container-img">
           <img src={item.coverImage} className="img-big" alt={item.name} />
@@ -27,6 +30,60 @@ export const ProductDetails = () => {
             <div>{item.name}</div>
             <div>
               {item.currency.symbol} {item.price}
+            </div>
+            <div className="action-area">
+              {cart.find((cartItem) => cartItem.id === item.id) ? (
+                <div className="text-addedToCart wishlist">
+                  <span className="icon-addedToCart">
+                    <IoBagCheckOutline />
+                  </span>
+                  Moved to Cart
+                </div>
+              ) : (
+                <div className="custom-container-btn-action">
+                  <button
+                    onClick={() => {
+                      cartDispatch({
+                        type: "ADD_TO_CART",
+                        payload: { ...item, quantity: 1 },
+                      });
+                      return setSnackbar({
+                        openStatus: true,
+                        type: "info",
+                        data: "Moved to cart",
+                      });
+                    }}
+                  >
+                    Move to cart
+                  </button>
+                </div>
+              )}
+
+              {wishlist.find((wishlistItem) => wishlistItem.id === item.id) ? (
+                <div
+                  className="custom-container-heart"
+                  onClick={() =>
+                    wishlistDispatch({
+                      type: "REMOVE_FROM_WISHLIST",
+                      payload: item.id,
+                    })
+                  }
+                >
+                  <AiFillHeart />
+                </div>
+              ) : (
+                <div
+                  className="custom-container-heart"
+                  onClick={() =>
+                    wishlistDispatch({
+                      type: "ADD_TO_WISHLIST",
+                      payload: item,
+                    })
+                  }
+                >
+                  <AiOutlineHeart />
+                </div>
+              )}
             </div>
           </div>
         </div>
